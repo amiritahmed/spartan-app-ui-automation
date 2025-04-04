275f365be756f0d0e29c5dedf5aee850ea2b4e51
amiritahmed <amiritahmed@gmail.com>
2025-04-04 19:15:05 -0400

    changed tags

--- src/test/java/com/devopinc/runners/CukesRunner.java
+++ src/test/java/com/devopinc/runners/CukesRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-              //  "json:target/cucumber.json",
-              //  "html:target/cucumber-reports.html",
+                "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
